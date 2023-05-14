5408cfdab3a0528656f522721a926454b656bb46
Jawhar <jawher.dhaou@esprit.tn>
2023-05-14 21:56:02 +0100

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Dhaou Jawhar",
+          to_name: "Jawhar",
           from_email: form.email,
-          to_email: "jawher.dhaou@esprit.tn",
+          to_email: "dhaou_jawher@hotmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
